d65c999c49f1ec59a833f5165cdbdd806915c36b
Wojciech Urbański <woid@pm.me>
2019-08-17 12:57:38 +0200

    Allow specifying own images for coordinator

--- docs/private_tangle/01_calculate_layers.sh
+++ docs/private_tangle/01_calculate_layers.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -o eu
 
 scriptdir=$(dirname "$(readlink -f "$0")")
 . $scriptdir/lib.sh
 
+image=${1:-iota/compass/docker:layers_calculator}
+
 load_config
 
-docker run -t --rm -v $scriptdir/data:/data iota/compass/docker:layers_calculator layers_calculator_deploy.jar -sigMode $sigMode -seed $seed -depth $depth -security $security -layers /data/layers
+docker run -t --rm -v $scriptdir/data:/data "${image}" layers_calculator_deploy.jar -sigMode $sigMode -seed $seed -depth $depth -security $security -layers /data/layers
--- docs/private_tangle/03_run_coordinator.sh
+++ docs/private_tangle/03_run_coordinator.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -o eu
 
 scriptdir=$(dirname "$(readlink -f "$0")")
 . $scriptdir/lib.sh
 
+image=${1:-iota/compass/docker:coordinator}
+
 load_config
 
-docker run -t --net host --rm -v $scriptdir/data:/data iota/compass/docker:coordinator coordinator_deploy.jar \
+docker run -t --net host --rm -v $scriptdir/data:/data "${image}" coordinator_deploy.jar \
 	-layers /data/layers \
 	-statePath /data/compass.state \
 	-sigMode $sigMode \
